54352f56702370bf7b8fc3466a9b122a6f96653a
elazo <elazo@CASA>
2025-10-15 16:07:03 -0500

    se creo el proyecto en maven java para la tarea de machine learning

---
+++ backprop-javafx/src/main/java/edu/backprop/backprop/javafx/BackpropJavafx.java
@@ -0,0 +1,11 @@
+package edu.backprop.backprop.javafx;
+/**
+ *
+ * @author elazo
+ */
+public class BackpropJavafx {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
